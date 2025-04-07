70ecd2c099faca6d238254f779e4b779a1931042
秦禧 <1362350354@qq.com>
2025-04-08 00:12:24 +0800

    feat:导航样式

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -9,7 +9,7 @@ const TabIcon = ({focused, icon, title} : any) => {
       return (
           <ImageBackground
               source={images.highlight}
-              className={'flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden'}
+              className={'flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden'}
           >
               <Image
                   source={icon} tintColor={'#151312'}
@@ -22,25 +22,39 @@ const TabIcon = ({focused, icon, title} : any) => {
       )
   }
   return (
-      <ImageBackground
-          source={images.highlight}
-          className={'flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden'}
-      >
+      <View className={'size-full justify-center items-center mt-4 rounded-full'}>
           <Image
-              source={icon} tintColor={'#151312'}
+              source={icon} tintColor={'#A8B5DB'}
               className={'size-5'}
           ></Image>
-          <Text className={'text-secondary text-base font-semibold ml-2'}
-          >{title}</Text>
-
-      </ImageBackground>
+      </View>
   )
 
 }
 const _Layout = () => {
   // @ts-ignore
     return (
-      <Tabs>
+      <Tabs
+          screenOptions={{
+              tabBarShowLabel: false,
+              tabBarItemStyle: {
+                  width: '100%',
+                  height: '100%',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+              },
+              tabBarStyle: {
+                  backgroundColor: '#0f0D23',
+                  borderRadius: 50,
+                  marginHorizontal: 20,
+                  height: 52,
+                  position: 'absolute',
+                  overflow: 'hidden',
+                  borderWidth: 1,
+                  borderColor: '#0f0D23',
+              }
+          }}
+      >
         <Tabs.Screen
             name="index"
             options={{
